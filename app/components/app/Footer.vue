deb59efed4c70c123e4c1eadd27740bccfb43559
Julian Lobbes <julian@lobbes.dev>
2025-08-02 00:34:23 +0100

    style: fix formatting

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -4,15 +4,13 @@ const year = ref(new Date().getFullYear());
 
 <template>
   <div
-    class="from-background-400 via-background-600 to-background-400 bg-gradient-to-b text-lg flex w-full items-center justify-start px-6 py-2"
+    class="from-background-400 via-background-600 to-background-400 flex w-full items-center justify-start bg-gradient-to-b px-6 py-2 text-lg"
   >
     <div class="flex items-center justify-center gap-1">
       <Icon name="mdi:copyright" />
-      <p class="font-light font-decor">
+      <p class="font-decor font-light">
         {{ year }}
-        <a href="https://github.com/ulinja/pastentonne">
-          Ulinja
-        </a>
+        <a href="https://github.com/ulinja/pastentonne"> Ulinja </a>
       </p>
     </div>
   </div>
